9dc4bf71c45a9a7e3bd0b4cc7e9b41a882ca6b9d
Bryn Smith <BrynSmith11@gmail.com>
2022-07-23 16:59:40 -0500

    Edited user input.

--- hackmidwest2022-ionic/src/app/zipcodepicker/zipcodepicker.page.ts
+++ hackmidwest2022-ionic/src/app/zipcodepicker/zipcodepicker.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl }  from '@angular/forms';
 
 @Component({
   selector: 'app-zipcodepicker',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ZipcodepickerPage implements OnInit {
 
+  ZipCodeControl = new FormControl('');
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  ZipCodeSubmit(): void {
+    console.log(this.ZipCodeControl.value);
+  }
+  
 }
