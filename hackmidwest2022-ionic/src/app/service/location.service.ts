f94cf8c084da555e1412d403c286d68a72a9b198
Caulfield Pearson <caulpearson@gmail.com>
2022-07-23 14:11:45 -0500

    Added to location service

--- hackmidwest2022-ionic/src/app/service/location.service.ts
+++ hackmidwest2022-ionic/src/app/service/location.service.ts
@@ -4,6 +4,27 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LocationService {
-
+  zipcode = '';
+  state = '';
+  city = '';
   constructor() { }
+
+  setState(state: string): void {
+    this.state = state;
+  }
+  setZipcode(zipcode: string): void {
+    this.zipcode = zipcode;
+  }
+  setCity(city: string): void {
+    this.city = city;
+  }
+  getState(): string {
+    return this.state;
+  }
+  getZipcode(): string {
+    return this.zipcode;
+  }
+  getCity(): string {
+    return this.city;
+  }
 }
