315cd8413ac724f2912d375730bc2209195da585
Caulfield Pearson <caulpearson@gmail.com>
2022-07-24 06:06:29 -0500

    Some small fixes

--- hackmidwest2022-ionic/src/app/home/treat/treatlocations/treatlocation/treatlocation.module.ts
+++ hackmidwest2022-ionic/src/app/home/treat/treatlocations/treatlocation/treatlocation.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -15,6 +15,7 @@ import { TreatlocationPage } from './treatlocation.page';
     IonicModule,
     TreatlocationPageRoutingModule
   ],
-  declarations: [TreatlocationPage]
+  declarations: [TreatlocationPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class TreatlocationPageModule {}
